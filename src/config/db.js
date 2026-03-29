bf9892f4559093fae967075c1b205b3eeda3e072
munir79 <jakirhossainmunir79@gmail.com>
2026-03-29 17:48:15 +0600

    done

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URL);
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error.message);
+    console.error("MongoDB Connection Failed:", error.message);
     process.exit(1);
   }
 };
